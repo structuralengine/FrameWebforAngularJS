eeed6bf9d1c534e054881107f12a2ee633897b71
sasaco <sasaicco@gmail.com>
2018-05-02 15:35:20 +0900

    完成

---
+++ app/scripts/controllers/_popup.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webframe.controller:PopupCtrl
+ * @description
+ * # BasicInformationCtrl
+ * Controller of the webframe
+ */
+angular.module('webframe')
+    .controller('PopupCtrl', ['$scope', function ($scope) {
+        $scope.ngPopupConfig = {
+            modelName: 'myNgPopup',
+            width: 600,
+            height: 500,
+            template: '<main ng-view></main>',
+            resizable: true,
+            draggable: true,
+            position: {
+                top: 100,
+                left: 100
+            },
+            title: '',
+            hasTitleBar: true,
+            pinned: false,
+            isShow: true,
+            onOpen: function () { },
+            onClose: function () { },
+            onDragStart: function () { },
+            onDragEnd: function () { },
+            onResize: function () { }
+        }
+    }]);
--- app/scripts/controllers/basic-information.js
+++ app/scripts/controllers/basic-information.js
@@ -8,17 +8,6 @@
  * Controller of the webframe
  */
 angular.module('webframe')
-  .controller('BasicInformationCtrl', ['$scope', function ($scope) {
-    $scope.ngPopupConfig = {
-      modelName: 'myNgPopup',
-      width: $scope.inputWidth,
-      height: $scope.inputHeight,
-      templateUrl: '../views/members.html',
-      resizable: true,
-      draggable: true,
-      position: { top: 500, left: 500 },
-      onOpen: function () {
-        /*Some Logic...*/
-      }
-    }
-  }]);
+  .controller('BasicInformationCtrl', function () {
+  });
+
