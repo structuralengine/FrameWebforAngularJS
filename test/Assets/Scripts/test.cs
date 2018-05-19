a330dd8957c130b7e9fa8c479510da8454dfd0db
sasaco <sasaicco@gmail.com>
2018-05-19 18:33:53 +0900

    unity連携

---
+++ test/Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        Application.ExternalCall("receiveUnity", "Button click!");
+    }
+    // JSからUnity内でイベント発火
+    public void UpdateTarget(string str)
+    {
+        Application.ExternalEval("alert('hoge');");
+    }
+}
